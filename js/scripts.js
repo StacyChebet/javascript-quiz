8ad51a89d912fee8bd379d2c6c09777fcef0c4c2
StacyChebet <staceychebet23@gmail.com>
2018-09-23 21:24:46 +0300

    change the values of each option to either true or false

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+//Business-logic:
+$ function quiz() {
+  var qn1= document.getELementById.("qn1").value;
+  var qn2= document.getELementById.("qn2").value;
+  var qn3= document.getELementById.("qn3").value;
+  var qn4= document.getELementById.("qn4").value;
+  var qn5= document.getELementById.("qn5").value;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
