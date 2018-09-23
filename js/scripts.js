da8a58605a9b522fb1305be40ae492c150da7d45
StacyChebet <staceychebet23@gmail.com>
2018-09-23 21:31:55 +0300

    set the if-else control flow for qn1

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
 //Business-logic:
-$ function quiz() {
-  var qn1= document.getELementById.("qn1").value;
-  var qn2= document.getELementById.("qn2").value;
-  var qn3= document.getELementById.("qn3").value;
-  var qn4= document.getELementById.("qn4").value;
-  var qn5= document.getELementById.("qn5").value;
+function check () {
+  var qn1= document.js-quiz.qn1.value;
+  var qn2= document.js-quiz.qn2.value;
+  var qn3= document.js-quiz.qn3.value;
+  var qn4= document.js-quiz.qn4.value;
+  var qn5= document.js-quiz.qn5.value;
+
+  var score= 10;
+
+  if (qn1=="true") {
+    score= 10;
+  } else {
+    score =0;
+  };
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
 
 
 
